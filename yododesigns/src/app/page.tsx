e2cde57f3a8c6a3f08998279c14a53f3440aa145
mustafatawab <engrmustafatawab05@gmail.com>
2024-02-05 18:55:17 +0500

    Yodo Designs

---
+++ yododesigns/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/ui/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ yododesigns/src/components/PrimaryButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrimaryButton = () => {
+  return (
+    <button>
+        
+    </button>
+  )
+}
+
+export default PrimaryButton
